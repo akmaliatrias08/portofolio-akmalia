295560d4a977df234fe0d9d4b0c20a1e3448aa0e
akmaliatrias08 <akmaliatrias08@gmail.com>
2025-03-02 13:15:57 +0700

    first commit

---
+++ data/biodata.js
@@ -0,0 +1,42 @@
+const biodata = {
+    avatar: "./assets/images/akmalia-ava.png",
+    name: "Akmalia Trias",
+    jobTitle: "Backend Developer",
+    contactList: [
+        {
+            icon: `<ion-icon name="mail-outline"></ion-icon>`, 
+            title: "Email", 
+            info: "akmaliatrias08@gmail.com",
+            url: "mailto:akmaliatrias08@gmail.com"
+        }, 
+        {
+            icon: `<ion-icon name="phone-portrait-outline"></ion-icon>`,
+            title: "Phone", 
+            info: "+6289647650752", 
+            url: "https://wa.me/+6289647650752"
+        },
+        {
+            icon: `<ion-icon name="location-outline"></ion-icon>`,
+            title: "Location", 
+            info: "Kota Bekasi", 
+            url: "https://maps.app.goo.gl/4dqLnCmt227LU1gL7"
+        }
+    ], 
+    socialMediaList: [
+        {
+            icon: `<ion-icon name="logo-instagram"></ion-icon>`, 
+            url: "https://www.instagram.com/akmaltrias_"
+        }, 
+        {
+            icon: `<ion-icon name="logo-linkedin"></ion-icon>`, 
+            url: "https://www.linkedin.com/in/akmalia-trias/"
+        }
+    ], 
+    about: `
+        Hello! I'm a backend developer with over 1 years of experience working on various projects for startups, corporations, and government.
+        </br>
+        </br>
+        With analytical problem-solving skills, I can deliver effective solutions through clear communication with team members and clients.
+        I am determined to keep learning and continuously improve in this industry.
+    `
+}
